9a3ab2ec3767a74110c827aac566ffef10ea9e3d
Ankhez <79629070381@yandex.ru>
2017-04-18 12:26:30 +0300

    Update PaginationAttention.py

--- PaginationAttention.py
+++ PaginationAttention.py
@@ -1,8 +1,24 @@
 import requests
 
+def checkthePagination(filename, url, **kwargs):
 
-def check(filename, f):
-  payload = {'rid':'dota2', 'ajax':'block_matches_current', 'data[s]': f, 'data[type]': 'gg'}
-  r = requests.post('http://game-tournaments.com/dota-2', data = payload)
-  with open(filename, 'w') as output_file:
-      output_file.write(r.text.encode('utf-8'))
+
+    r = requests.post(url, data = kwargs)
+    with open(filename, 'w') as output_file:
+        output_file.write(r.text.encode('utf-8'))
+
+count = 1
+dct = {
+
+    'rid':'dota2',
+    'ajax':'block_matches_current',
+    'data[s]': count,
+    'data[type]' :'gg'
+
+}
+
+
+
+
+if __name__ == '__main__':
+    checkthePagination('kurqa.html', 'http://game-tournaments.com/dota-2', **dct)
